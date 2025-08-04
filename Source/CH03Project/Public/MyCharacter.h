c98068e374f00c9f79dd55c6c4d2e742a8b1dbb7
GKH-a <whitegrub173@gmail.com>
2025-08-04 19:40:51 +0900

    [추가] 캐릭터, 컨트롤러, IA, IMC

---
+++ Source/CH03Project/Public/MyCharacter.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyCharacter.generated.h"
+
+class UCameraComponent;
+struct FInputActionValue;
+
+
+UCLASS()
+class CH03PROJECT_API AMyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+protected:
+	// 컴포넌트
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	USceneComponent* SceneComp;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	USkeletalMeshComponent* SkeletalMeshComp;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	UCameraComponent* CameraComp;
+
+	// 포인터 변수
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation")
+	UAnimInstance* AnimInstance;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+	UAnimMontage* FireMontage;
+
+	// 일반 변수
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "State")
+	bool bIsShooting;
+
+	// 타이머
+	FTimerHandle ShootTimerHandle;
+
+
+
+public:
+	AMyCharacter();
+
+	// Getter, Setter
+	UFUNCTION(BlueprintCallable, Category = "State")
+	bool GetbIsShooting();
+
+	// 일반 함수
+	void Shoot();
+
+
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void Move(const FInputActionValue& value);
+
+	void Look(const FInputActionValue& value);
+
+	void StartShoot(const FInputActionValue& value);
+
+	void StopShoot(const FInputActionValue& value);
+};
