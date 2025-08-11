c5a226ae3ef7288084373059079de8fcff6aa8ba
DiooN <dioon0000@naver.com>
2025-08-11 16:50:39 +0900

    [기능] CardKey, CatStatue | BaseItem 상속 C++ 코드 추가, ItemDataTable 텍스처 및 임시사운드 추가

---
+++ Source/CH03Project/Private/Items/CatStatue.cpp
@@ -0,0 +1,6 @@
+﻿#include "Items/CatStatue.h"
+
+void UCatStatue::Use_Implementation(AActor* User)
+{
+	// 고양이 사운드 출력
+}
---
+++ Source/CH03Project/Public/Items/CardKey.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/BaseItem.h"
+#include "CardKey.generated.h"
+
+UCLASS()
+class CH03PROJECT_API UCardKey : public UBaseItem
+{
+	GENERATED_BODY()
+	
+	virtual void Use_Implementation(AActor* User) override;
+};
