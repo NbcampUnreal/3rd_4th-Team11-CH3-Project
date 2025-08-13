0565167b1ee26d1c890a8810db95a023a853e949
oSiris1o7 <siuesen@gmail.com>
2025-08-13 18:24:28 +0900

    [기능] 인벤토리 드래그앤드랍

---
+++ Source/CH03Project/Public/DragMoveWidget.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Image.h"
+#include "DragMoveWidget.generated.h"
+
+
+UCLASS()
+class CH03PROJECT_API UDragMoveWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	//이미지의 텍스쳐 변경
+	UFUNCTION(BlueprintCallable, Category = "ImageChange")
+	void ImageChange(UTexture2D* NewImage, UImage* ImageWidget);
+	
+};
---
+++ Source/CH03Project/Public/InventoryItemSlot.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "InventoryItemSlot.generated.h"
+
+
+UCLASS()
+class CH03PROJECT_API UInventoryItemSlot : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation) override;
+
+    // 드래그할 아이템의 고유 ID를 저장합니다.
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    FName ItemID;
+
+    // 아이템의 타입을 저장합니다 (예: A, B, C 파츠).
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    FName ItemType;
+
+    // 아이템의 아이콘 이미지를 저장합니다.
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
+    class UTexture2D* ItemIcon;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DragDrop")
+    TSubclassOf<UUserWidget> DragVisualWidgetClass;
+};
